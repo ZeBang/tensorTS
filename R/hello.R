801078198a510b69d66cb73ce1db73f26f4fda18
zebang <942189149@qq.com>
2020-01-02 16:55:24 +0800

    basic structure in R folder

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,6 @@
-# Hello, world!
+# add
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
+# This is an example function named 'add'
 #
 # You can learn more about package authoring with RStudio at:
 #
@@ -13,6 +12,18 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+
+#' Add together two numbers.
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+add <- function(x, y) {
+  x + y
 }
+
+# -----To Ruo fan: edit your function BELOW-----
+
